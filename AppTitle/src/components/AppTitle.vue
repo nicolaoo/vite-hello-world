80898cce7d7e672837627a6364f9f4ab90526d59
nicola00 <nicolajobs00@gmail.com>
2023-02-28 15:35:11 +0100

    FEAT new components

---
+++ AppTitle/src/components/AppTitle.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div>
+        <h1 class="first-text">
+            {{ name }}
+        </h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'questa è la mia prima pagina su vite!!'
+        }
+    }
+}
+
+</script>
+
+
+<style>
+.first-text {
+    background-color: cadetblue;
+}
+</style>
